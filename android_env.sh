57f47dbc6fadfe066f0ce1f227a3514921e21b07
qmlsxxxx <qmlsxxxx@gmail.com>
2020-04-06 19:35:22 +0800

    update apt instsall packages for ubuntu-18.04

--- android_env.sh
+++ android_env.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # required packages
-sudo add-apt-repository ppa:webupd8team/java
+#sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install git gnupg flex bison gperf build-essential \
   zip curl libc6-dev x11proto-core-dev \
   libx11-dev:i386 libreadline6-dev:i386 \
-  libgl1-mesa-dev g++-multilib mingw32 tofrodos \
+  libgl1-mesa-dev g++-multilib tofrodos \
   python-markdown libxml2-utils xsltproc zlib1g-dev:i386 \
   ccache unzip mutt ctags gawk dos2unix
 #sudo apt-get install openjdk-7-jdk
 #sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
 
 # install java
-sudo apt-get install oracle-java6-installer
+#sudo apt-get install oracle-java6-installer
 
 echo "export USE_CCACHE=1" >> $HOME/.bashrc
 echo "export CCACHE_DIR=$HOME/.ccache" >> $HOME/.bashrc
--- ubuntu_env.sh
+++ ubuntu_env.sh
@@ -5,12 +5,11 @@ cp Consolas.ttf $HOME/.fonts/
 sudo fc-cache -f -v
 
 sudo dpkg-reconfigure dash
-sudo add-apt-repository ppa:tsvetko.tsvetkov/cinnamon
 sudo apt-add-repository ppa:git-core/ppa
 sudo apt-get update
 
 sudo apt-get install vim samba openssh-server vnc4server \
-	cinnamon p7zip-full screen subversion libncurses5-dev ctags \
+	p7zip-full screen subversion libncurses5-dev ctags \
 	diffstat chrpath texinfo
 
 cp .vimrc $HOME/.vimrc
