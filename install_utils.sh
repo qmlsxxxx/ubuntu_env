ca5ebb5235e3680dc53319e72a6a20467712caa3
ivan_chung <ivan_chung@realtek.com>
2023-12-28 21:10:00 +0800

    add bash-completion

--- install_utils.sh
+++ install_utils.sh
@@ -5,4 +5,4 @@ source common.sh
 sudo $apt install vim samba smbclient openssh-server vnc4server \
 	p7zip-full screen subversion ctags htop silversearcher-ag \
 	lbzip2 pxz cpufrequtils zssh bindfs cifs-utils ffmpeg tig \
-	axel tree ccache nfs-kernel-server 
+	axel tree ccache nfs-kernel-server bash-completion
